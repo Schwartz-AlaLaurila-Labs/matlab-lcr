1fd71533e31166ebd9bc2d7668bc54f10076042c
Sam Cooler <scooler@gmail.com>
2017-03-10 17:21:56 -0600

    fix pattern color lookup

--- LightCrafter4500.m
+++ LightCrafter4500.m
@@ -162,7 +162,7 @@ classdef LightCrafter4500 < handle
                 insertBlack = false;
                 trigOutPrev = false;
                 
-                colorIndex = cellfun(@(c)strncmpi(c, color{i}, length(color{i})), obj.LEDS);
+                colorIndex = cellfun(@(c)strcmp(c, color{i}), obj.LEDS);
                 ledSelect = find(colorIndex, 1, 'first') - 1;
                 
                 lcrAddToPatLut(trigType, patNum, bitDepth, ledSelect, invertPat, insertBlack, bufSwap, trigOutPrev);
